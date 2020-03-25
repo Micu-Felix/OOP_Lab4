2ce8ef2adbeecfe4eb689e1e539425b8626f0050
Micu-Felix <felix.claudiu@yahoo.de>
2020-03-25 20:31:23 +0200

    just start 3

--- Apotheke.cpp
+++ Apotheke.cpp
@@ -4,41 +4,39 @@
 #include "Apotheke.h"
 #include <algorithm>
 #include <iostream>
+#include <string.h>
 
 void Apotheke::add_med(std::string name, float concentrare, int anzahl, float pret) {
-    int already_exists=0;
-    for( auto iter=medicamente.begin(); iter !=  medicamente.end(); iter++)
-    {
-        if ((*iter)->get_name() == name&&(*iter)->get_concentrare()==concentrare)
-        {
-            already_exists=1;
-            (*iter)->set_anz((*iter)->get_anz()+anzahl);
+    int already_exists = 0;
+    for (auto iter = medicamente.begin(); iter != medicamente.end(); iter++) {
+        if ((*iter)->get_name() == name && (*iter)->get_concentrare() == concentrare) {
+            already_exists = 1;
+            (*iter)->set_anz((*iter)->get_anz() + anzahl);
         }
     }
-    if(!already_exists)
-    {
-     medicamente.push_back(new Medikation(name,concentrare,anzahl,pret));
+    if (!already_exists) {
+        medicamente.push_back(new Medikation(name, concentrare, anzahl, pret));
     }
 
 }
 
 void Apotheke::anzeigen(std::string substring) {
-    for( auto iter=medicamente.begin(); iter !=  medicamente.end(); iter++)
-    {
-        std::cout<<"Das Medikament: "<<(*iter)->get_name()<<" mit der Konzentration: "<<(*iter)->get_concentrare()<<'\n';
-    }
+    if (substring=="" )
+        for (auto iter = medicamente.begin(); iter != medicamente.end(); iter++) {
+            std::cout << "Das Medikament: " << (*iter)->get_name() << " mit der Konzentration: "
+                      << (*iter)->get_concentrare() << '\n';
+        }
 }
 
 void Apotheke::knapp() {
     int x;
-    std::cout<<"Knapp bedeutet kleiner als: ";
-    std::cin>>x;
-    std::cout<<'\n'<<"Die Medikamente die knapp sind, sind: "<<'\n';
-    for( auto iter=medicamente.begin(); iter !=  medicamente.end(); iter++)
-    {
-        if ((*iter)->get_anz() < x)
-        {
-            std::cout<<"Das Medikament: < "<<(*iter)->get_name()<<" > mit der Konzentration: "<<(*iter)->get_concentrare()<<'\n';
+    std::cout << "Knapp bedeutet kleiner als: ";
+    std::cin >> x;
+    std::cout << '\n' << "Die Medikamente die knapp sind, sind: " << '\n';
+    for (auto iter = medicamente.begin(); iter != medicamente.end(); iter++) {
+        if ((*iter)->get_anz() < x) {
+            std::cout << "Das Medikament: < " << (*iter)->get_name() << " > mit der Konzentration: "
+                      << (*iter)->get_concentrare() << '\n';
         }
     }
 }
--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include "Apotheke.h"
 int main() {
-    std::cout << "Hello Lab4" << std::endl;
+    std::cout << "Hello Lab4!" << std::endl;
     Apotheke Smile;
     Smile.add_med("Xanax",50,21,300);
     Smile.add_med("Xanax",50,21,300);
     Smile.add_med("Paracetamol",25,2,30);
-    Smile.anzeigen("ceva");
+    Smile.anzeigen("");
     Smile.knapp();
     return 0;
 }
